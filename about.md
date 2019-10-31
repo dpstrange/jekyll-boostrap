6952a6fe13c23f551559dba9020ac79139440e9b
Drew Parrish Strange <drew@strange.onl>
2019-10-31 01:08:08 -0500

    Initial Commit

---
+++ about.md
@@ -0,0 +1,12 @@
+---
+layout: page
+title: About
+description: "About Jekyll Bootstrap"
+---
+
+# Inspiration
+
+Jekyll Bootstrap was inspired by a handful of great wizards.
+
+- <a href="https://mademistakes.com/" target="_blank">Michael Rose - Made Mistakes</a>
+- <a href="https://huangxuan.me/" target="_blank">Huang Xuan - Hux Blog</a>
---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* Hello World */
+
+console.log('Jekyll Bootstrap - https://github.com/dpstrange/jekyll-bootstrap')
+
+/* Anchor.js */
+
+anchors.options = {
+    icon: '#'
+};
+anchors.add('.anchors h1, .anchors h2, .anchors h3, .anchors h4, .anchors h5, .anchors h6');
+
+/* Externalize Links */
+$(document).ready(function(){
+    // resolve external links in new tab
+    $('a[href^="http://"], a[href^="https://"]').not('a[href*="'+location.hostname+'"]').attr('target','_blank').addClass("external");
+});
