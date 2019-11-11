3271e85d204f03d2be497251dc49c32b6dbd18e5
Drew Parrish Strange <dpstrange@lilith.local>
2019-11-11 00:53:36 -0600

    Pet peeves

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@ anchors.options = {
 anchors.add('.anchors h1, .anchors h2, .anchors h3, .anchors h4, .anchors h5, .anchors h6');
 
 /* Externalize Links */
+
 $(document).ready(function(){
     // resolve external links in new tab
     $('a[href^="http://"], a[href^="https://"]').not('a[href*="'+location.hostname+'"]').attr('target','_blank').addClass("external");
